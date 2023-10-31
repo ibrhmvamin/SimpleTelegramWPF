33e397e9c73d9eb5099154ab17d313741a1d105d
ibrhmvamin <aminibrahimov52@gmail.com>
2023-10-31 11:33:36 +0400

    SimpleTelegram

---
+++ SimpleTelegramWPF/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SimpleTelegramWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MyMouseenter(object sender, MouseEventArgs e)
+        {
+            if (txtbox.Text == "Write a message...")
+                txtbox.Text = "";
+        }
+
+        private void MyMouseLeave(object sender, MouseEventArgs e)
+        {
+            txtbox.Text = "Write a message...";
+        }
+
+        private void SearchClick(object sender, MouseButtonEventArgs e)
+        {
+            if (searchtxtbox.Text == "Search")
+                searchtxtbox.Text = "";
+        }
+    }
+}
